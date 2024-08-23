f703f420932fe12acb8fbd2451ff1273b49fea25
Furkan <ilfuryca@gmail.com>
2024-08-24 01:32:39 +0300

    reverse fetchGames to see production error

--- src/app/api/fetchGames/route.ts
+++ src/app/api/fetchGames/route.ts
@@ -1,3 +1,26 @@
-import { createApiHandler } from "../apiHandler";
+// import { createApiHandler } from "../apiHandler";
 
-export const GET = createApiHandler("/games");
+// export const GET = createApiHandler("/games");
+
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+  const { search } = new URL(req.url);
+  const apiUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/games${search || "?"}key=${apiKey}`;
+
+  try {
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      throw new Error(`Response error: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return NextResponse.json({ error: "Failed to fetch data" }, { status: 500 });
+  }
+}
