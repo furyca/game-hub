16b1d656e4cef5f011fb317ac0669d08d8b27a0a
Furkan <ilfuryca@gmail.com>
2024-09-21 21:20:44 +0300

    improve cls 1

--- src/components/GameDetails/GameDetails.tsx
+++ src/components/GameDetails/GameDetails.tsx
@@ -20,7 +20,7 @@ import useWideScreen from "@/hooks/useWideScreen";
 import useClient from "@/hooks/useClient";
 import { setGameData } from "@/lib/features/singleGame/singleGameSlice";
 
-const GameDetails = ({ data }: {data: any}) => {
+const GameDetails = ({ data }: { data: any }) => {
   const dispatch = useAppDispatch();
   const wideScreen = useWideScreen();
   const client = useClient();
@@ -30,33 +30,31 @@ const GameDetails = ({ data }: {data: any}) => {
   }, []);
 
   return (
-    <div className="flex justify-center mb-32">
-      <div className="w-full lg:w-auto mx-auto max-w-[480px] lg:max-w-[960px]">
-        <Breadcrumb />
-        <div className="flex w-full max-w-[480px] lg:max-w-[960px] flex-col lg:flex-row">
-          {/* Left part */}
-          <div className="w-full">
-            <GameMeta />
-            {wideScreen || <GameScreenshots />}
-            <UserActions />
-            {wideScreen || <EditGame />}
-            <Ratings />
-            <RatingBars />
-            <RatingButtons />
-            <CommentReview />
-            <AboutGame />
-            <QuickInfos />
-            <SystemReqs />
-          </div>
-          {/* Right part */}
-          <div className="lg:ms-12 lg:w-96">
-            {wideScreen && <GameScreenshots />}
-            {wideScreen && <EditGame />}
-            <Stores />
-          </div>
+    <div className="w-full lg:w-[960px] mx-auto max-w-[480px] lg:max-w-[960px]">
+      <Breadcrumb />
+      <div className="flex flex-col lg:flex-row">
+        {/* Left part */}
+        <div>
+          <GameMeta />
+          {wideScreen || <GameScreenshots />}
+          <UserActions />
+          {wideScreen || <EditGame />}
+          <Ratings />
+          <RatingBars />
+          <RatingButtons />
+          <CommentReview />
+          <AboutGame />
+          <QuickInfos />
+          <SystemReqs />
         </div>
-        {client && createPortal(<GameBackground />, document.body)}
+        {/* Right part */}
+        <div className="lg:ms-12 lg:w-96">
+          {wideScreen && <GameScreenshots />}
+          {wideScreen && <EditGame />}
+          <Stores />
+        </div>
       </div>
+      {client && createPortal(<GameBackground />, document.body)}
     </div>
   );
 };
