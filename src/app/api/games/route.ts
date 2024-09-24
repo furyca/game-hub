a95012861d24b1c263424a0d73d39c4bee34a63a
Furkan <ilfuryca@gmail.com>
2024-09-24 22:25:17 +0300

    correct api route name

--- src/app/api/games/route.ts
+++ src/app/api/games/route.ts
@@ -3,8 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
   const { search } = new URL(req.url);
   const id = search.slice(1);
-  const apiUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}${id}?key=${process.env.NEXT_PUBLIC_API_KEY}`;
-  const ssUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}${id}/screenshots?key=${process.env.NEXT_PUBLIC_API_KEY}`;
+  const apiUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/games/${id}?key=${process.env.NEXT_PUBLIC_API_KEY}`;
+  const ssUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/games/${id}/screenshots?key=${process.env.NEXT_PUBLIC_API_KEY}`;  
 
   const responses = await Promise.all([fetch(apiUrl), fetch(ssUrl)]);
   const data = await Promise.all(responses.map((response) => response.json()));
--- src/lib/features/singleGame/singleGameSlice.ts
+++ src/lib/features/singleGame/singleGameSlice.ts
@@ -30,19 +30,11 @@ const initialState: SingleGameProps = {
 };
 
 export const getSingleGame = createAsyncThunk("getSingleGame", async (id: string) => {
-  const response = await fetch(`/api/getSingleGame?/games/${id}`);
+  const response = await fetch(`/api/games?${id}`);
 
   if (!response.ok) {
     throw new Error("Response error");
   }
-
-  //temporary direct call
-  // const apiUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/games/${id}?key=${process.env.NEXT_PUBLIC_API_KEY}`;
-  // const ssUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/games/${id}/screenshots?key=${process.env.NEXT_PUBLIC_API_KEY}`;
-
-  // const responses = await Promise.all([fetch(apiUrl), fetch(ssUrl)]);
-  // const data = await Promise.all(responses.map((response) => response.json()));
-
 
   const data = await response.json();
 
